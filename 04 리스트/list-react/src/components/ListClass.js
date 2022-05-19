893c584ca9e9df918ce4596bfa3c8f6d171dfacb
nahee101 <bagsj26@gmail.com>
2022-05-19 09:12:32 +0900

    third commit

--- 04 리스트/list-react/src/components/ListClass.js
+++ 04 리스트/list-react/src/components/ListClass.js
@@ -1,117 +1,102 @@
-import {Component} from 'react';
+import { Component } from "react";
 
-/* map list */
 class ListClass extends Component {
     constructor(props) {
         super(props);
         // 값을 배열로 받아옴
-        this.seasons = ['봄', '여름', '가을', '겨울'];
-        // map을 통해서 동일한 컴포넌트나 엘리먼트에 넣음 -> 새로운 배열로 저장
-        this.listItems = this.seasons.map(
-            (season, index) => <li key={index}>{season}</li>
-            );
+        this.numbers = ["봄", "여름", "가을", "겨울"];
+        // map을 통해서 동일한 컴포넌트 또는 엘리먼트에 넣어서 새로운 배열로 저장
+        this.listItems = this.numbers.map((number, index) => <li key={index}>{number}</li>);
 
-        // state의 값을 비구조화할당
-        // 배열 값을 사용할 때는 key로 쓸 수 있는 id 값을 추가한다
         this.state = {
-            planets: [
-                {id: 1, text: '수성'},
-                {id: 2, text: '금성'},
-                {id: 3, text: '지구'},
-                {id: 4, text: '화성'},
-                {id: 5, text: '목성'},
-                {id: 6, text: '토성'},
-                {id: 7, text: '천왕성'},
-                {id: 8, text: '해왕성'}
-            ],
-            /* 받아올 값 */
-            inputText: {text: '', id: '',},
-            /* 학생 목록을 table 태그로 출력 */
-            students: [
-                {id: 1, name: '원더우먼'},
-                {id: 2, name: '배트맨'},
-                {id: 3, name: '슈퍼맨'}
-            ]
+        // 배열값을 사용해줄때는 키 값을 위한 id 값을 추가
+        season: [
+            { id: 1, text: "봄" },
+            { id: 2, text: "여름" },
+            { id: 3, text: "가을" },
+            { id: 4, text: "겨울" },
+        ],
+        // 값을 받아 올 text와 id
+        inputId: 5,
+        inputText: "",
+        // 아래 학생을 table 태그로 출력하세요
+        students: [
+            { id: 1, name: "홍길동" },
+            { id: 2, name: "성춘향" },
+            { id: 3, name: "Jhon" },
+        ],
         };
+    }
+    // 함수를 들고와서 클래스형에서 사용할수 있도록 수정
+    deleteSeason = (id) => {
+        const nextSeason = this.state.season.filter((s) => s.id !== id);
+        // 클래스형에서는 setState를 통해서 값을 수정한다.
+        this.setState({ season: nextSeason });
     };
-
-        /*deletePlanet = (id) => {
-        const nextPlanet = this.state.planets.filter(p => p.id !== id);
-            // 클래스형에서는 setState를 통해서 값을 수정한다
-            this.setState({planets: nextPlanet})
-        };*/
 
     // 바뀐 값을 가져오는 함수
-    changeText = (e) => {
-        this.setState({[e.target.name]: e.target.value})
-    };
+    // setState()를 사용할 때는 항상 {(state에 작성한 속성이름):값} 으로 넣어준다.
+    // 값 : 1, "문자열", 변수로 넣어둔 값
+    changeText = (e) => this.setState({ [e.target.name]: e.target.value });
 
     // 배열에 값을 넣는 함수
     getText = () => {
-        //this.state.planets.push(this.state.inputText);
-        // this.setState({planets: nextPlanet});
-        const nextPlanet = this.state.planets.concat({
-            id:this.state.inputId, 
-            text:this.state.inputText
+        // 아래 값은 state의 값을 직접적으로 바꾸기때문에 권장하지않는다.
+        //this.state.season.push(this.state.inputText);
+
+        // concat을 이용하여 두개의 값이 더한 배열을 만든다.
+        const nextSeason = this.state.season.concat({
+        id: this.state.inputId,
+        text: this.state.inputText,
         });
-        this.setState({inputId: this.state.inputId +1});
-        this.setState({planets: nextPlanet});
+
+        this.setState({ season: nextSeason });
+        // prevState를 통해 this.state의 값을 가져와서 사용하였다
+        this.setState((prevState) => ({ inputId: prevState.inputId + 1 }));
     };
 
     render() {
-        const {planets} = this.state;
-        const {students} = this.state;
-        // const listPlanet = planets.map(p => <li key={p.id}>{p.text}</li>);
+        const { season, students } = this.state;
+        const listSeason = season.map((s) => <li key={s.id}>{s.text}</li>);
 
-        return(
-            <div>
-                {/* 사계 */}
-                <div>
-                    <ul>{this.listItems}</ul>
-                </div>
+        return (
+        <div>
+            <input type="text" name="inputText" onChange={this.changeText}></input>
+            <button onClick={this.getText}>추가</button>
+            <ul>{listSeason}</ul>
+            <ol>
+            {season.map((s) => (
+                <li
+                key={s.id}
+                onClick={() => {
+                    this.deleteSeason(s.id);
+                }}
+                >
+                {s.text}
+                </li>
+            ))}
+            </ol>
 
-                {/* 행성 */}
-                <div>
-                    <input type='text' name='inputText'
-                    onChange={this.changeText}>
-                    </input>
-                    <button onClick={this.getText}>
-                        추가
-                    </button>
-
-                    <ul>
-                        {planets.map((p) => (
-                        <li key={p.id} 
-                        //함수에 값을 전달하기 위해서는 익명 함수로 감싸야 함
-                        >
-                            {p.text}
-                        </li>
-                        ))}
-                    </ul>
-                </div>
-
-                {/* 출석부 */}
-                <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>아이디</td>
-                                <td>이름</td>
-                            </tr>
-
-                            {students.map((student) => (
-                                // 여러줄로 작성할 때 ()로 묶어 return
-                                <tr>
-                                    <td>{student.id}</td>
-                                    <td>{student.name}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        )
-    };
-};
-
+            <table>
+            <tbody>
+                <tr>
+                <td>아이디</td>
+                <td>이름</td>
+                </tr>
+                {students.map(
+                (
+                    student // 여러줄로 작성할때 ()묶어서 return으로 보냄
+                ) => (
+                    <tr key={student.id}>
+                    <td>{student.id}</td>
+                    <td>{student.name}</td>
+                    </tr>
+                )
+                )}
+            </tbody>
+            </table>
+        </div>
+        );
+    }
+}
 export default ListClass;
--- 04 리스트/list-react/src/components/ListFunction.js
+++ 04 리스트/list-react/src/components/ListFunction.js
@@ -1,40 +1,37 @@
-/* 함수형 */
-
 import { useState } from "react";
-const listFunction = () => {
-    // useState를 통해서 state를 작성함
-    const [planet, setPlanet] = useState([
-        // key 값을 위해서 id를 작성한다
-        {id: 1, text: '수성'},
-        {id: 2, text: '금성'},
-        {id: 3, text: '지구'},
-        {id: 4, text: '화성'},
-        {id: 5, text: '목성'},
-        {id: 6, text: '토성'},
-        {id: 7, text: '천왕성'},
-        {id: 8, text: '해왕성'}
+const ListFunction = () => {
+    //useState통해서 state 작성
+    const [season, setSeason] = useState([
+        // key값 위해서 id를 작성
+        { id: 1, text: "봄" },
+        { id: 2, text: "여름" },
+        { id: 3, text: "가을" },
+        { id: 4, text: "겨울" },
     ]);
 
-    // id 값을 받아음 -> 받아온 id를 제외한 값을 저장한다
-    // = id를 삭제하는 것과 동일한 효과
-    const deletePlanet = (id) => {
-        const nextPlanet = planet.filter(p => p.id !== id);
-        setPlanet(nextPlanet);
+    // id 값을 받아와서 받아온 id를 제외한 값을 저장하는 함수
+    // 받아온 id 값만 제외하였기에 id 를 삭제하는 것과 동일
+    const deleteSeason = (id) => {
+        const nextSeason = season.filter((s) => s.id !== id);
+        setSeason(nextSeason);
     };
 
     return (
         <div>
-            <ul>
-                {planet.map((p) => (
-                    <li key={p.id} 
-                    //함수에 값을 전달하기 위해서는 익명 함수로 감싸야 함
-                    onClick={() => {deletePlanet(p.id)}}>
-                        {p.text}
-                    </li>
-                ))}
-            </ul>
+        <ul>
+            {season.map((s) => (
+            <li
+                key={s.id}
+                onClick={() => {
+                // 함수에 값을 전달하기위해서는 익명함수로 감싸서 사용
+                deleteSeason(s.id);
+                }}
+            >
+                {s.text}
+            </li>
+            ))}
+        </ul>
         </div>
     );
 };
-
-export default listFunction;
+export default ListFunction;
